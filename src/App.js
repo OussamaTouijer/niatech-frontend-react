0e8e637b2ed7305b5d2f9efcde5ecfdfb3082db4
TOUIJER OUSSAMA <oussama5touijer@gmail.com>
2025-04-16 07:40:55 +0100

    v4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Footer from './components/Footer/Footer';
 import Container from './components/Container/Container';
 import Card from './sections/Card/Card'; 
 import Banner from './sections/Banner/Banner';
-import Features from './sections/Features/Features';
 //import About from './sections/About/About';
 
 
@@ -14,9 +13,9 @@ import Features from './sections/Features/Features';
 function App() {
   return (
     <div>
-    <Navbar/>
-      <Container>
+      <Navbar/>
 
+      <Container>
         <Banner/>
         <Card/>
 
